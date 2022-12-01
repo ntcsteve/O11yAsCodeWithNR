56e8aec8e19f428ae21c77847025b1cf0af25722
Steve Ng <ntc.steve@outlook.com>
2022-12-01 09:06:26 +0700

    updated

--- FoodMe/2.Alerts/variables.tf
+++ FoodMe/2.Alerts/variables.tf
@@ -1,20 +1,24 @@
+# your unique New Relic account ID 
 variable "nr_account_id" {
   default = "XXXXX"
 }
 
+# your User API key
 variable "nr_api_key" {
   default = "XXXXX"
 }
 
-# Valid regions are US and EU
+# valid regions are US and EU
 variable "nr_region" {
   default = "US"
 }
 
+# your unique New Relic App ID 
 variable "nr_appname" {
   default = "FoodMe-XXXXX"
 }
 
+# your email address to send notification 
 variable "nr_email" {
   default = "XXXXX"
 }
